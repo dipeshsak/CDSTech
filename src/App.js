5b1d48911e9d87bfa475fbfb6e099eb73540a164
Dipesh <dipesh.sakharkar@otiselevator.com>
2021-02-14 17:51:18 +0530

    Base Code

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import HomePage from "./Pages/HomePage"
+import './App.css'
+const App = () =>{
+  return(
+   <div className="App">
+     <h1>App Component</h1>
+     <HomePage />
+   </div>
+  )
+}
+
+export default App;
